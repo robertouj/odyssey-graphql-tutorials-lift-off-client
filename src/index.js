4f9854c8207f52d3463b35a4462154dae48d5932
Roberto Ubieto Juan <robertouj@gmail.com>
2022-03-20 02:22:48 +0100

    change url to production

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.REACT_APP_SERVER_URL || "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
@@ -14,5 +14,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
